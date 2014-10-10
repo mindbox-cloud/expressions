aec6239633bd956fe20df72355dd4ba09db212dc
Ihar Bury <Ihar.Bury@gmail.com>
2014-10-10 20:33:04 +0400

    Separate symbols package

--- Mindbox.Expressions/package.cmd
+++ Mindbox.Expressions/package.cmd
@@ -4,4 +4,5 @@
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=Release45 /t:Rebuild
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=ReleaseSL4 /t:Rebuild
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:Configuration=ReleaseSL5 /t:Rebuild
-NuGet pack
+NuGet pack -Symbols
+NuGet pack -Exclude **/*.pdb
