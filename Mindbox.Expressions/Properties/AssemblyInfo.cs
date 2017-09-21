4f7acdea7a52109b02c93cf21e5b220f9a15f8cb
Roman Ivonin <ivonin@mindbox.ru>
2017-09-21 20:26:49 +0300

    Preparing for publishing 3.0 version (.netstandard compatible)

--- Mindbox.Expressions/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyDescription("Allows to include lambda expressions into each other in c# and Visual Basic. " +
-	"Also allows to avoid using string constants in reflection thus enabling easy refactoring.")]
