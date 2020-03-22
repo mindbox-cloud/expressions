98ae786a1c030d05df6f74523ed24e2b69711a9f
Рудневский Артем <rudnevskiy@corp.itcd.ru>
2020-03-22 16:16:04 +0300

    CachingCompilingExpressionEvaluator

---
+++ Mindbox.Expressions/Evaluators/Caching/ClosureCapturedValuesProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Mindbox.Expressions
+{
+	internal class ClosureCapturedValuesProvider : ClosureCapturedValuesVisitor
+	{
+		public static object[] GetCapturedValues(Expression expression)
+		{
+			var visitor = new ClosureCapturedValuesProvider();
+			visitor.Visit(expression);
+
+			return visitor.capturedValues.ToArray();
+		}
+
+		private ClosureCapturedValuesProvider()
+		{
+
+		}
+
+		private readonly List<object> capturedValues = new List<object>();
+
+		protected override Expression TryProcessClosure(ConstantExpression node)
+		{
+			if (node == null)
+				throw new ArgumentNullException(nameof(node));
+
+			capturedValues.Add(node.Value);
+
+			return null;
+		}
+	}
+}
---
+++ Mindbox.Expressions/Evaluators/Caching/ClosureCapturedValuesVisitor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Mindbox.Expressions
+{
+	internal abstract class ClosureCapturedValuesVisitor : ExpressionVisitor
+	{
+		protected ClosureCapturedValuesVisitor()
+		{
+
+		}
+
+		protected sealed override Expression VisitConstant(ConstantExpression node)
+		{
+			if (node == null)
+				throw new ArgumentNullException(nameof(node));
+
+			if (node.Value == null)
+				return base.VisitConstant(node);
+
+			var replacedQuery = TryProcessClosure(node);
+			if (replacedQuery != null)
+				return replacedQuery;
+
+			return base.VisitConstant(node);
+		}
+
+		protected abstract Expression TryProcessClosure(ConstantExpression node);
+	}
+}
